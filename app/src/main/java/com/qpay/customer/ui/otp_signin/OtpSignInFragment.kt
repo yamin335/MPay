f2ca548aa1d378744cd478c7d6e459edac772e4e
Ibrahim Khalil <khalil.ibrahim@bjitgroup.com>
2020-08-25 11:55:04 +0600

    OTP pin validatation & permission
     - OTP pin validation for length of 6
     - Asking for permissions to the user
     - Ted permission library added.

--- app/src/main/java/com/qpay/customer/ui/otp_signin/OtpSignInFragment.kt
+++ app/src/main/java/com/qpay/customer/ui/otp_signin/OtpSignInFragment.kt
@@ -1,18 +1,24 @@
 package com.qpay.customer.ui.otp_signin
 
+import android.Manifest
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.view.WindowManager
 import androidx.fragment.app.viewModels
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.qpay.customer.BR
 import com.qpay.customer.R
 import com.qpay.customer.databinding.OtpSignInBinding
 import com.qpay.customer.ui.NavigationHost
 import com.qpay.customer.ui.common.BaseFragment
 
-class OtpSignInFragment : BaseFragment<OtpSignInBinding, OtpSignInViewModel>() {
+class OtpSignInFragment : BaseFragment<OtpSignInBinding, OtpSignInViewModel>(), PermissionListener {
 
     override val bindingVariable: Int
         get() = BR.viewModel
@@ -26,6 +32,13 @@ class OtpSignInFragment : BaseFragment<OtpSignInBinding, OtpSignInViewModel>() {
     var START_TIME_IN_MILLI_SECONDS = 180000L
     lateinit var countdown_timer: CountDownTimer
     var repeater = 0
+    override fun onPermissionGranted() {
+        navController().navigate(OtpSignInFragmentDirections.actionOtpSignInFragmentToPinNumberFragment())
+    }
+
+    override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -61,12 +74,45 @@ class OtpSignInFragment : BaseFragment<OtpSignInBinding, OtpSignInViewModel>() {
             host.registerToolbarWithNavigation(this)
         }
         viewDataBinding.btnSubmit.setOnClickListener {
-            navController().navigate(OtpSignInFragmentDirections.actionOtpSignInFragmentToPinNumberFragment())
+            TedPermission.with(requireContext())
+                .setPermissionListener(this)
+                .setDeniedMessage(getString(R.string.if_you_reject_these_permission_the_app_wont_work_perfectly))
+                .setPermissions(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.READ_SMS,
+                    Manifest.permission.RECEIVE_SMS
+                ).check()
         }
 
         viewDataBinding.btnResend.setOnClickListener {
             startTimer(START_TIME_IN_MILLI_SECONDS)
         }
+
+        viewDataBinding.btnSubmit.isEnabled = false
+        viewDataBinding.btnSubmit.setBackgroundColor(Color.parseColor("#808080"))
+        viewDataBinding.etOtpCode.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (viewDataBinding.etOtpCode.text.toString().length == 6) {
+                    viewDataBinding.btnSubmit.isEnabled = true
+                    viewDataBinding.btnSubmit.setBackgroundColor(Color.parseColor("#70DB44"))
+                } else {
+                    viewDataBinding.btnSubmit.isEnabled = false
+                    viewDataBinding.btnSubmit.setBackgroundColor(Color.parseColor("#808080"))
+                }
+            }
+
+        })
+
+
     }
 
 //    private fun pauseTimer() {
